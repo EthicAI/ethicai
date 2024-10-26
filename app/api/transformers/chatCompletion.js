6af0a39098e1a29ca3e134a3230dea0c31312fb7
yoarajota <joaovbscontato@gmail.com>
2024-10-26 14:15:06 -0300

    fix: ajust dotenv and api

--- app/api/transformers/chatCompletion.js
+++ app/api/transformers/chatCompletion.js
@@ -1,38 +1,37 @@
-import Groq from 'groq-sdk';
+import Groq from "groq-sdk";
 
 export default class ChatCompletion {
+  _parseResponseStream = async (chatCompletionResponse) => {
+    let response = [];
+    for await (const chunk of chatCompletionResponse) {
+      response += chunk.choices[0]?.delta?.content || "";
+    }
 
-	_parseResponseStream = async (chatCompletionResponse) => {
-		let response = [];
-		for await (const chunk of chatCompletionResponse) {
-			response += chunk.choices[0]?.delta?.content || '';
-		}
+    return response;
+  };
 
-		return response;
-	}
+  generate = async (task) => {
+    const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-	generate = async task => {
-		const grok = new Groq({ apiKey: process.env.GROQ_API_KEY });
-		const apiResponse = await grok.chat.completions.create({
-			messages: [
-				{
-					role: "system",
-					content: `Se tarefa "${task}" for considerada ética, retorne um passo a passo em tópicos de como posso realizá-la eticamente, caso contrário, retorne uma mensagem de erro.`
-				},
-				{
-					role: "user",
-					content: ""
-				}
-			],
-			model: "llama3-8b-8192",
-			temperature: 1,
-			max_tokens: 1024,
-			top_p: 1,
-			stream: true,
-			stop: null
-		});
+    const apiResponse = await groq.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: `Você é um consultor e especialista sobre filosofia e ética. Sua função é ajudar os usuários a avaliar a ética de suas ações e fornecer orientações detalhadas sobre como agir de maneira ética.`,
+        },
+        {
+          role: "user",
+          content: `Se a tarefa "${task}" for considerada ética, retorne um passo a passo em tópicos de como posso realizá-la eticamente, caso contrário, retorne uma mensagem de erro.`,
+        },
+      ],
+      model: "llama3-8b-8192",
+      temperature: 1,
+      max_tokens: 1024,
+      top_p: 1,
+      stream: true,
+      stop: null,
+    });
 
-		return await this._parseResponseStream(apiResponse);
-	}
-
-};
+    return await this._parseResponseStream(apiResponse);
+  };
+}
