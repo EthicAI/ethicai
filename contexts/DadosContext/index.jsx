ed1947e0844aef2fbdccc20c0f1516a6b17d5a35
Juliano Possamai <julianop.possamai@gmail.com>
2024-11-10 11:44:12 -0300

    feat: persist conversation in localStorage

--- contexts/DadosContext/index.jsx
+++ contexts/DadosContext/index.jsx
@@ -1,29 +1,35 @@
 "use client";
 
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer, useEffect } from "react";
 import { ActionList, listReducer } from "@/util/listUtils";
-
 
 export const DadosProviderContext = createContext({
   conversa: [],
-  addConversa: (e) => { },
-  removeConversa: (e) => { }
+  addConversa: (e) => {},
+  removeConversa: (e) => {}
 });
 
 export function DadosProvider({ children }) {
   const [conversa, dispatch] = useReducer(listReducer, []);
 
+  useEffect(() => {
+    const storedConversa = JSON.parse(localStorage.getItem("conversa")) || [];
+    storedConversa.forEach(item => dispatch({ type: ActionList.ADD_ITEM, item }));
+  }, []);
+
   const addConversa = (item) => {
     dispatch({ type: ActionList.ADD_ITEM, item });
+    localStorage.setItem("conversa", JSON.stringify([...conversa, item]));
   };
 
   const removeConversa = (item) => {
     dispatch({ type: ActionList.REMOVE_ITEM, item });
+    localStorage.setItem("conversa", JSON.stringify(conversa.filter(i => i !== item)));
   };
 
   return (
     <DadosProviderContext.Provider
-      value={{ conversa: conversa, addConversa: addConversa, removeConversa: removeConversa }}
+      value={{ conversa, addConversa: addConversa, removeConversa: removeConversa }}
     >
       {children}
     </DadosProviderContext.Provider>
