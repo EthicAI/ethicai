2ae04f2ea5528ce78333fe5fbe3dc49fd575fc8a
Juliano Possamai <julianop.possamai@gmail.com>
2024-11-10 14:16:38 -0300

    refactor: const for localStorage key

--- contexts/DadosContext/index.jsx
+++ contexts/DadosContext/index.jsx
@@ -9,22 +9,24 @@ export const DadosProviderContext = createContext({
   removeConversa: (e) => {}
 });
 
+const localStorageKey = "__ethicAiHistory";
+
 export function DadosProvider({ children }) {
   const [conversa, dispatch] = useReducer(listReducer, []);
 
   useEffect(() => {
-    const storedConversa = JSON.parse(localStorage.getItem("conversa")) || [];
+    const storedConversa = JSON.parse(localStorage.getItem(localStorageKey)) || [];
     storedConversa.forEach(item => dispatch({ type: ActionList.ADD_ITEM, item }));
   }, []);
 
   const addConversa = (item) => {
     dispatch({ type: ActionList.ADD_ITEM, item });
-    localStorage.setItem("conversa", JSON.stringify([...conversa, item]));
+    localStorage.setItem(localStorageKey, JSON.stringify([...conversa, item]));
   };
 
   const removeConversa = (item) => {
     dispatch({ type: ActionList.REMOVE_ITEM, item });
-    localStorage.setItem("conversa", JSON.stringify(conversa.filter(i => i !== item)));
+    localStorage.setItem(localStorageKey, JSON.stringify(conversa.filter(i => i !== item)));
   };
 
   return (
