096e908c4917a150d564cc115630f3f2299c33ee
Juliano Possamai <julianop.possamai@gmail.com>
2024-11-10 11:55:24 -0300

    feat: auto-scroll to bottom of messages container on new messages

--- components/MessagesContainer/index.jsx
+++ components/MessagesContainer/index.jsx
@@ -1,15 +1,26 @@
 "use client";
+import { useEffect, useRef } from "react";
 import { MessageBox } from "../CustomComponents";
 import { useDados } from "@/contexts/DadosContext";
 
 export default function MessagesContainer() {
   const { conversa: messages } = useDados();
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   return (
     <div className="flex-grow p-5 overflow-y-auto">
       {messages.map((message, index) => (
         <MessageBox key={index} message={message} />
       ))}
+      <div ref={messagesEndRef} />
     </div>
   );
 }
