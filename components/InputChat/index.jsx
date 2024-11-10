0226828d63a057ab2f0c183d5a7b94608c941002
Juliano Possamai <julianop.possamai@gmail.com>
2024-11-10 11:53:32 -0300

    fix: question submission pressing enter

--- components/InputChat/index.jsx
+++ components/InputChat/index.jsx
@@ -9,7 +9,8 @@ export default function InputChat() {
   const { addConversa, removeConversa } = useDados();
   const [pergunta, setPergunta] = useState("");
 
-  const buscarResposta = () => {
+  const buscarResposta = event => {
+    event.preventDefault();
     addConversa(pergunta);
 
     enviarPergunta(pergunta, (resposta) => {
@@ -20,7 +21,7 @@ export default function InputChat() {
   };
 
   return (
-    <div className="flex items-center p-2 bg-[#654525b3] rounded-full w-full max-w-3xl border border-[#F5A524]">
+    <form onSubmit={buscarResposta} className="flex items-center p-2 bg-[#654525b3] rounded-full w-full max-w-3xl border border-[#F5A524]">
       <input
         type="text"
         value={pergunta}
@@ -29,12 +30,12 @@ export default function InputChat() {
         className="flex-grow p-3 bg-[#3a3a1d] text-[#e1a917] placeholder-[#e1a917] rounded-full border-none focus:outline-none"
       />
       <button
-        onClick={buscarResposta}
+        type="submit"
         className="ml-3 px-4 py-2 bg-[#e1a917] text-[#3a3a1d] rounded-full hover:bg-[#c89b0f] flex items-center justify-center transition-colors shadow-md"
       >
         <span className="mr-1">Enviar</span>
         <ArrowUpIcon />
       </button>
-    </div>
+    </form>
   );
 }
