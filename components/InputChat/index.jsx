9cb5339b757f99cbe30dae47fa9daf2a6b53350b
João Vítor Sberse <81939995+yoarajota@users.noreply.github.com>
2024-11-26 13:45:32 -0300

    feat: using shadcn (#19)

--- components/InputChat/index.jsx
+++ components/InputChat/index.jsx
@@ -4,13 +4,16 @@ import { TIPO_CONVERSA, useDados } from "@/contexts/DadosContext";
 import { enviarPergunta } from "@/util/requests/UtilTransformers";
 import { ArrowUpIcon, ReloadIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function InputChat() {
-  const { addConversa, removeConversa, conversa } = useDados();
+  const { addConversa, conversa } = useDados();
   const [pergunta, setPergunta] = useState("");
   const [gerouResposta, setGerouResposta] = useState(false);
 
-  const buscarResposta = event => {
+  const buscarResposta = (event) => {
     event.preventDefault();
     addConversa({ tipo: TIPO_CONVERSA.PERGUNTA, msg: pergunta });
     setGerouResposta(false);
@@ -24,7 +27,7 @@ export default function InputChat() {
 
   const regerarResposta = () => {
     setGerouResposta(false);
-    let perguntas = conversa.filter(i => i.tipo == TIPO_CONVERSA.PERGUNTA);
+    let perguntas = conversa.filter((i) => i.tipo == TIPO_CONVERSA.PERGUNTA);
     enviarPergunta(`${perguntas[perguntas.length - 1].msg}`, (resposta) => {
       addConversa({ tipo: TIPO_CONVERSA.RESPOSTA, msg: resposta });
       setGerouResposta(true);
@@ -34,30 +37,36 @@ export default function InputChat() {
   };
 
   return (
-    <form onSubmit={buscarResposta} className="flex items-center p-2 bg-[#654525b3] rounded-full w-full max-w-3xl border border-[#F5A524]">
-      <input
-        type="text"
-        value={pergunta}
-        onChange={(e) => setPergunta(e.target.value)}
-        placeholder="Coloque aqui a sua pergunta"
-        className="flex-grow p-3 bg-[#3a3a1d] text-[#e1a917] placeholder-[#e1a917] rounded-full border-none focus:outline-none"
-      />
-      <button
-        type="submit"
-        className="ml-3 px-4 py-2 bg-[#e1a917] text-[#3a3a1d] rounded-full hover:bg-[#c89b0f] flex items-center justify-center transition-colors shadow-md"
-      >
-        <span className="mr-1">Enviar</span>
-        <ArrowUpIcon />
-      </button>
-      {
-        gerouResposta &&
-        <button
-          onClick={regerarResposta}
-          className="ml-3 px-2 py-2 bg-[#e1a917] text-[#3a3a1d] rounded-full hover:bg-[#c89b0f] flex items-center justify-center transition-colors shadow-md"
+    <form
+      onSubmit={buscarResposta}
+      className="flex w-full items-center max-w-3xl"
+    >
+      <Card className="flex items-center p-2 w-full">
+        <Input
+          type="text"
+          value={pergunta}
+          onChange={(e) => setPergunta(e.target.value)}
+          placeholder="Coloque aqui a sua pergunta"
+          className="flex-grow"
+        />
+        <Button
+          onClick={buscarResposta}
+          className="ml-3 flex items-center justify-center"
+          type="submit"
         >
-          <ReloadIcon />
-        </button>
-      }
+          <span className="mr-1">Enviar</span>
+          <ArrowUpIcon />
+        </Button>
+        {gerouResposta && (
+          <Button
+            onClick={regerarResposta}
+            className={"ml-3"}
+            variant="outline"
+          >
+            <ReloadIcon />
+          </Button>
+        )}
+      </Card>
     </form>
   );
 }
--- components/MessagesContainer/index.jsx
+++ components/MessagesContainer/index.jsx
@@ -3,13 +3,19 @@
 import { useEffect, useRef } from "react";
 import { MessageBox } from "../CustomComponents";
 import { useDados } from "@/contexts/DadosContext";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { MessageSquarePlus } from "lucide-react";
+import { ScrollArea } from "../ui/scroll-area";
 
 export default function MessagesContainer() {
   const { conversa: messages } = useDados();
   const messagesEndRef = useRef(null);
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scroll({ top: messagesEndRef.current.scrollHeight, behavior: "smooth" });
+    messagesEndRef.current?.scroll({
+      top: messagesEndRef.current.scrollHeight,
+      behavior: "smooth",
+    });
   };
 
   useEffect(() => {
@@ -17,10 +23,29 @@ export default function MessagesContainer() {
   }, [messages]);
 
   return (
-    <div className="flex-grow p-5 overflow-y-auto" ref={messagesEndRef}>
-      {messages.map((message, index) => (
-        <MessageBox key={index} message={message.msg} tipo={message.tipo} />
-      ))}
+    <div
+      className="flex-grow p-5 overflow-y-auto relative"
+      ref={messagesEndRef}
+    >
+      {messages.length === 0 ? (
+        <Card className="w-full max-w-md absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <CardHeader className="text-center">
+            <CardTitle>Sem mensagens ainda...</CardTitle>
+          </CardHeader>
+          <CardContent className="text-center">
+            <MessageSquarePlus className="mx-auto h-12 w-12 text-muted-foreground" />
+            <p className="mt-4 text-sm text-muted-foreground">
+              Comece uma conversa!
+            </p>
+          </CardContent>
+        </Card>
+      ) : (
+        <ScrollArea className="max-w-3xl mx-auto pr-4 h-full">
+          {messages.map((message, index) => (
+            <MessageBox key={index} message={message.msg} tipo={message.tipo} />
+          ))}
+        </ScrollArea>
+      )}
     </div>
   );
 }
